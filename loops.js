64ea7412cd26113119aa2eb99b5b1ad9476ffa8a
Chau Tran <cctran96@gmail.com>
2021-02-20 06:28:28 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -8,25 +8,3 @@ function forLoop(array) {
   return array
 }
 
-function whileLoop(n) {
-  let countdown = n;
-
-  while (countdown > 0) {
-    console.log(--countdown)
-  }
-    return 'done'
-}
-
-
-function doWhileLoop(num) {
-  let i = 0;
-
-  function incrementVariable() {
-    i = i + 1;
-    return i;
-  }
-
-  do {
-    console.log("I run once regardless.");
-  } while (incrementVariable() < num);
-}
